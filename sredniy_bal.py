0c0ec981ff05ff31e9551064ada39aaa88882345
ljusien <ljusien777@gmail.com>
2024-11-17 22:07:43 +0300

    Add files via upload

---
+++ sredniy_bal.py
@@ -0,0 +1,25 @@
+first_name='Aaron'
+print(first_name)
+set = [5, 3, 3, 5, 4]
+set = [(5+3+3+5+4)/5]
+print(set)
+second_name='Bilbo'
+set = [2, 2, 2, 3]
+set = [(2+2+2+3)/4]
+print('Bilbo')
+print(set)
+third_name='Johnny'
+set = [4,5,5,2]
+set = [(4+5+5+2)/4]
+print(third_name)
+print(set)
+fourth_name='Khendrik'
+print(fourth_name)
+set=[4,4,3]
+set=[(4+4+3)/3]
+print(set)
+fifth_name='Steve'
+print(fifth_name)
+set=[5, 5, 5, 4, 5]
+set=[(5+5+5+4+5)/5]
+print(set)
